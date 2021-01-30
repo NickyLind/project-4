8ab701d1398fb0adf0e1c9bbea5dfed131b5db78
Nick Lindau <nicholaithegreat@gmail.com>
2021-01-29 16:23:39 -0800

    fix issues with displaying receipt

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,5 @@
 // first I will create a database-like object called 'Database' that holds objects called orders,
 //and IDs so that i can go back and pick out orders by their IDs. Next I will create an object called 
-// 'Orders' that will store one or more Pizza objects i create so people can order multiple pizzas.
-// Next I will create a pizza object that will allow users to create a base pizza style, size, and 
-// additional toppings. I will use a prototype for 'Database' called assignOrderId to add an id to 
-// each order. I will use a prototype for 'Database' called addOrder to add an Order object into
-// the database. I will create an Order prototype called addPizza to add pizzas to an order.
-// I will create a Pizza prototype called customPizza to add sizes and toppings to pizzas.
-// I will create arrays for topping tiers. I will assign a price to each tier of toppings and 
-// loop through these arrays to add dollar values each time a topping in each tier is selected
-// I will create an if statement for pizza sizes that adds or subtracts from a 'medium' pizza for 
-// different sizes
-
 // Business logic for Database
 function Database() {
   this.orders = {}
@@ -93,14 +82,14 @@ $(document).ready(function()  {
     inputtedSize = $("#pizzaSize").val();
     inputtedType = $("#pizzaType").val(); 
     inputtedToppings = $("#pizzaToppings").val();
-    $("#pizzaOrder")[0].reset();
     let newPizza = new Pizza(inputtedType, inputtedSize, inputtedToppings)
     newPizza.Price();
     newDatabase.orders[1].addPizza(newPizza) 
     $("#pizzaOrder").hide(); 
     $("#showOrder").show(); 
     $("#receipt").show();
-    $(".receiptPrice").text("$" + newDatabase.orders[1].pizzas[1].price) 
+    $(".receiptName").text("Order Name: " + newDatabase.orders[1].name)
+    $(".receiptPrice").text("Price: $" + newDatabase.orders[1].pizzas[1].price) 
   })
 });
 
